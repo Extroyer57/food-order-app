0228662581998fef305e3b38b4e1b2e545cadfd7
Extroyer57 <sphesihlekhulu57@gmail.com>
2025-07-22 04:46:50 +1200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react'
+import MenuPage from './pages/MenuPage'
+
+function App() {
+  return (
+    <div>
+      <MenuPage />
+    </div>
+  )
+}
+
+export default App
